40a8a688c3f39f2fd00ae91e3fab8d5d4e60a705
khex <khex@ua.fm>
2023-06-26 21:55:04 +0300

    ci(actions): add broken text

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,5 @@
 const calculator = require('./calculator')
 
 test('string with a single number should result in the number itself', () => {
-  expect(calculator.add('1')).toBe(1);
+  expect(calculator.add('1')).toBe(10);
 });
